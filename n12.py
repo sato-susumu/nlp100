8ec9bef16ecb312e0ddb29740c64a66de182c6d5
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2021-12-11 17:52:59 +0900

    n12 clear

---
+++ n12.py
@@ -0,0 +1,18 @@
+import os
+
+path = './data/popular-names.txt'
+out_path1 = 'col1.txt'
+out_path2 = 'col2.txt'
+with open(path, 'r') as fr, \
+        open(out_path1, 'w') as fw1, \
+        open(out_path2, 'w') as fw2:
+    for line in fr.readlines():
+        data = line[:-1].split('\t')
+        fw1.write(data[0] + '\n')
+        fw2.write(data[1] + '\n')
+
+os.system(f'cat {path} | cut -f1 > col1-2.txt')
+os.system(f'cat {path} | cut -f2 > col2-2.txt')
+
+os.system(f'diff col1.txt col1-2.txt')
+os.system(f'diff col2.txt col2-2.txt')
