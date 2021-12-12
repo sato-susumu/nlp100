798c7ad53ee18eb2116ddc632542b39cb0368c3c
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2021-12-12 20:09:30 +0900

    n17 clear

--- n17.py
+++ n17.py
@@ -1,12 +1,19 @@
 import os
+from functools import cmp_to_key
+
+
+def compare(a, b):
+    a_2 = int(a.split('\t')[2])
+    b_2 = int(b.split('\t')[2])
+    return a_2 - b_2
 
 
 path = './data/popular-names.txt'
-N = 5
 with open(path) as f:
     lines = f.readlines()
-    name_set = set([line.split('\t')[0] for line in lines])
+    new_lines = sorted(lines, key=cmp_to_key(compare), reverse=True)
+    for line in new_lines:
+        print(line[:-1])
 
-print(name_set)
-print('')
-os.system(f' cut -f 1 {path} | sort | uniq')
+
+os.system(f'sort -r -n -k 3 {path}')
