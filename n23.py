c5aa17ef3077b0c4ec0d9823dfbb95e8fabad480
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2021-12-15 05:25:41 +0900

    n23 clear

---
+++ n23.py
@@ -0,0 +1,10 @@
+import re
+
+from n21 import get_england_lines
+
+for line in get_england_lines():
+    obj = re.search(r'(={2,})\s*([^=]*)\s*(={2,})', line)
+    if obj:
+        level = len(obj.group(1)) - 1
+        name = obj.group(2)
+        print(f'{level} {name}')
