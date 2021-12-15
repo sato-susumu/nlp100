626c4f76a1d61bd973748d118a3b5167a9627c08
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2021-12-15 09:03:33 +0900

    n27 clear

---
+++ n27.py
@@ -0,0 +1,16 @@
+import re
+from pprint import pprint
+
+from n21 import get_england_lines
+
+basic_dic = {}
+for line in get_england_lines():
+    obj = re.search(r'^\|(.*?)\s+=\s+(.*)', line)
+    if obj:
+        field_name = obj.group(1)
+        value = re.sub(r'\'{2,5}', '', obj.group(2))
+        value = re.sub(r'\[\[([^\]]*?)\|[^\]]*?]]', r'\1', value)
+        value = re.sub(r'\[\[([^\]]*?)]]', r'\1', value)
+        basic_dic[field_name] = value
+
+pprint(basic_dic)
