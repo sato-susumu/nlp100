db8b10a874f1efe74f65a20b13f395f81282b6b5
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2021-12-21 16:07:45 +0900

    update knock45

--- knock45.py
+++ knock45.py
@@ -9,23 +9,22 @@ if __name__ == '__main__':
     out_path = 'knock45.txt'
     with open(out_path, 'w') as fw:
         for line in result:
-            dependency_dict = defaultdict(lambda: [])
             for chunk in line:
-                if chunk.dst == -1:
+                if len(chunk.srcs) == 0:
                     continue
-                dest_chunk = line[chunk.dst]
-                if not chunk.has_particle():
+                if not chunk.has_verb():
                     continue
-                if not dest_chunk.has_verb():
-                    continue
-                particle = chunk.get_particle(-1).surface
-                base = dest_chunk.get_verb(0).base
-                # print(f'{particle}\t{base}')
-                if particle not in dependency_dict[base]:
-                    dependency_dict[base].append(particle)
+                base = chunk.get_verb(0).base
+                particle_list = []
+                for src in chunk.srcs:
+                    if not line[src].has_particle():
+                        continue
+                    particle = line[src].get_particle(-1).surface
 
-            for key, value_list in dependency_dict.items():
-                fw.write(f'{key} {" ".join(value_list)}\n')
+                    if particle not in particle_list:
+                        particle_list.append(particle)
+
+                fw.write(f'{base} {" ".join(particle_list)}\n')
 
     # コーパス中で頻出する「述語と格パターンの組み合わせ」
     os.system(f'sort {out_path} | uniq -c | sort -nr | head -n 10')
