18de04f6e4e2acf2b6619818aaa72f4891304ae6
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2021-12-21 13:10:43 +0900

    knock44 clear

---
+++ knock44.py
@@ -0,0 +1,24 @@
+from knock41 import load_ai_ja_txt_parsed
+from graphviz import Digraph
+
+if __name__ == '__main__':
+    result = load_ai_ja_txt_parsed()
+
+    dg = Digraph(format='png')
+    line_no = 2
+
+    # ノード作成
+    for i, chunk in enumerate(result[line_no]):
+        if chunk.dst == -1 and len(chunk.srcs) == 0:
+            continue
+        dg.node(str(i), label=f"{i}:{chunk.surface}")
+
+    # エッジ作成
+    for i, chunk in enumerate(result[line_no]):
+        if chunk.dst == -1:
+            continue
+        # dest_chunk = result[line_no][chunk.dst]
+        # print(f'{chunk.surface}\t{dest_chunk.surface}')
+        dg.edge(str(i), str(chunk.dst))
+
+    dg.render(None, view=True)
