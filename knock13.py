c8aa98f7e1cbfd790263c0ea03cc7785708b9819
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2021-12-28 15:57:48 +0900

    pandasを使うように変更

--- knock13.py
+++ knock13.py
@@ -1,17 +1,13 @@
 import os
+import pandas as pd
 
 in_path1 = 'col1.txt'
 in_path2 = 'col2.txt'
 out_path1 = 'merge.txt'
-
-with open(in_path1, 'r') as fr1, \
-        open(in_path2, 'r') as fr2, \
-        open(out_path1, 'w') as fw1:
-    fr1_lines = fr1.readlines()
-    fr2_lines = fr2.readlines()
-    for fr1_line, fr2_line in zip(fr1_lines, fr2_lines):
-        fw1.write(f'{fr1_line[:-1]}\t{fr2_line[:-1]}\n')
-
+df_name = pd.read_csv(in_path1, header=None, names=['name'])
+df_sex = pd.read_csv(in_path2, header=None, names=['sex'])
+df = pd.concat([df_name, df_sex], axis=1)
+df.to_csv(out_path1, index=False, header=False, sep='\t')
 
 os.system('paste col1.txt col2.txt > merge_cl.txt')
 os.system('diff merge.txt merge_cl.txt')
