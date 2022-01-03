cfb1d87dc97cc3ba8f73834cadad40719cb9843e
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2022-01-03 14:06:16 +0900

    knock61 clear. update knock60

--- knock61.py
+++ knock61.py
@@ -3,6 +3,5 @@ from gensim.models.keyedvectors import KeyedVectors
 # path = './data/googlenews-vectors-negative300.bin.gz'
 path = './data/googlenews-vectors-negative300.bin'
 model = KeyedVectors.load_word2vec_format(path, binary=True)
-vector = model['United_States']
-print(f'United_States vector: {vector}')
-print(f'  length = {len(vector)}')
+result = model.similarity('United_States', 'U.S.')
+print(f'similarity(United_States, U.S.) = {result}')
